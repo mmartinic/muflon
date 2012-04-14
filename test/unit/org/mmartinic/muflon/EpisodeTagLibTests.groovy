67973fa98989b3fe6f1a652313bc9ee045b30b02
Marin Martinic <corax34@gmail.com>
2012-04-14 19:22:44 +0200

    added unit tests for EpisodeTagLib and LoginTagLib

--- test/unit/org/mmartinic/muflon/EpisodeTagLibTests.groovy
+++ test/unit/org/mmartinic/muflon/EpisodeTagLibTests.groovy
@@ -1,9 +1,12 @@
 package org.mmartinic.muflon
 
-
-
 import grails.test.mixin.*
+
+import org.joda.time.LocalDate
 import org.junit.*
+import org.mmartinic.muflon.model.Episode
+import org.mmartinic.muflon.model.Episode.EpisodeKey;
+import org.mmartinic.muflon.model.Show
 
 /**
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
@@ -11,7 +14,30 @@ import org.junit.*
 @TestFor(EpisodeTagLib)
 class EpisodeTagLibTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testEpisodeNumber() {
+        LocalDate date = new LocalDate()
+        Show show = new Show(id: 1, name: "show")
+        EpisodeKey episodeKey = new EpisodeKey(show: show, seasonNumber: 1, episodeNumber: 2)
+        Episode episode = new Episode(name: "episode", airDate: date, episodeKey: episodeKey)
+
+        assertEquals ("01x02", applyTemplate('<g:episodeNumber episode="${episode}"/>', [episode: episode]))
+    }
+
+    void testIsohuntLink() {
+        LocalDate date = new LocalDate()
+        Show show = new Show(id: 1, name: "show")
+        EpisodeKey episodeKey = new EpisodeKey(show: show, seasonNumber: 1, episodeNumber: 2)
+        Episode episode = new Episode(name: "episode", airDate: date, episodeKey: episodeKey)
+
+        assertEquals ("<a href='http://isohunt.com/torrents/?ihq=show+S01E02'><img src=\"/images/isohunt.ico\"></a>", applyTemplate('<g:isohuntLink episode="${episode}"/>', [episode: episode]))
+    }
+
+    void testAddic7edLink() {
+        LocalDate date = new LocalDate()
+        Show show = new Show(id: 1, name: "show")
+        EpisodeKey episodeKey = new EpisodeKey(show: show, seasonNumber: 1, episodeNumber: 2)
+        Episode episode = new Episode(name: "episode", airDate: date, episodeKey: episodeKey)
+
+        assertEquals ("<a href='http://www.addic7ed.com/search.php?search=show+S01E02&Submit=Search'><img src=\"/images/addic7ed.ico\"></a>", applyTemplate('<g:addic7edLink episode="${episode}"/>', [episode: episode]))
     }
 }
---
+++ test/unit/org/mmartinic/muflon/LoginTagLibTests.groovy
@@ -0,0 +1,35 @@
+package org.mmartinic.muflon
+
+import static org.junit.Assert.*
+
+import grails.plugins.springsecurity.SpringSecurityService
+import grails.test.mixin.*
+import grails.test.mixin.support.*
+import groovy.mock.interceptor.StubFor
+
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+@TestFor(LoginTagLib)
+class LoginTagLibTests {
+
+    void testUserLoggedIn() {
+        def mockCreator = new StubFor(SpringSecurityService)
+        mockCreator.demand.isLoggedIn() { true }
+        mockCreator.demand.getCurrentUser() { [username:"Frank"] }
+
+        tagLib.springSecurityService = mockCreator.proxyInstance()
+
+        assertEquals ("Hello Frank[<a href=\"/logout\">Logout</a>]", applyTemplate('<g:loginControl />'))
+    }
+    void testNotUserLoggedIn() {
+        def mockCreator = new StubFor(SpringSecurityService)
+        mockCreator.demand.isLoggedIn() { false }
+
+        tagLib.springSecurityService = mockCreator.proxyInstance()
+
+        assertEquals ("[<a href=\"/login\">Login</a>]", applyTemplate('<g:loginControl />'))
+    }
+}
