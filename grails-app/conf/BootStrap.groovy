9d7fb6eeace603e7c9ead961ded2c9d5a6b5b5e1
Marin Martinic <corax34@gmail.com>
2012-09-13 23:43:25 +0200

    changed log4j config; change home page; added prod bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -13,18 +13,16 @@ class BootStrap {
 
         MetaClassInjector.init()
 
+        def adminRole = new Role(authority: Role.ADMIN).save(flush: true)
+        def userRole = new Role(authority: Role.USER).save(flush: true)
+        def user = new User(username: 'corax', enabled: true, password: 'change.me').save(flush: true)
+        UserRole.create user, adminRole, true
+        assert Role.count() == 2
+        assert User.count() == 1
+        assert UserRole.count() == 1
+
         switch(GrailsUtil.environment) {
             case "development":
-                def adminRole = new Role(authority: Role.ADMIN).save(flush: true)
-                def userRole = new Role(authority: Role.USER).save(flush: true)
-
-                def testUser = new User(username: 'me', enabled: true, password: 'password').save(flush: true)
-
-                UserRole.create testUser, adminRole, true
-
-                assert Role.count() == 2
-                assert User.count() == 1
-                assert UserRole.count() == 1
                 break
             case "production" :
                 break
