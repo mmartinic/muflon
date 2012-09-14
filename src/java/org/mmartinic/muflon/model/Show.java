4720a18a205c1f17327dc5193a1848b4aede7d52
Marin Martinic <corax34@gmail.com>
2012-09-14 23:43:00 +0200

    removed name from hashcode and equals for Show

--- src/java/org/mmartinic/muflon/model/Show.java
+++ src/java/org/mmartinic/muflon/model/Show.java
@@ -44,7 +44,6 @@ public class Show implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
 
@@ -60,12 +59,8 @@ public class Show implements Serializable {
         if (id == null) {
             if (other.id != null)
                 return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
+        }
+        else if (!id.equals(other.id))
             return false;
         return true;
     }
