b73c769dccf9bf678ea0588dffa5a1dde9d79743
Marin Martinic <corax34@gmail.com>
2012-09-18 22:58:52 +0200

    upgraded to grails 2.1.1; changed war name; changed data source configuration

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,26 +7,26 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.region.factory_class = "net.sf.ehcache.hibernate.EhCacheRegionFactory"
 }
 // environment specific settings
 environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost:3306/muflon?autoreconnect=true"
+            url = "jdbc:mysql://localhost:3306/muflon"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:mysql://localhost:3306/muflon?autoreconnect=true"
+            url = "jdbc:mysql://localhost:3306/muflon"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:mysql://localhost:3306/muflon?autoreconnect=true"
+            url = "jdbc:mysql://localhost:3306/muflon"
             pooled = true
             properties {
                 maxActive = -1
