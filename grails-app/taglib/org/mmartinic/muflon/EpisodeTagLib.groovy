24e750ab169ae5ea409edc494d07b221328ce55a
Marin Martinic <corax34@gmail.com>
2012-10-13 02:03:11 +0200

    added text next to links on episode list

--- grails-app/taglib/org/mmartinic/muflon/EpisodeTagLib.groovy
+++ grails-app/taglib/org/mmartinic/muflon/EpisodeTagLib.groovy
@@ -4,32 +4,32 @@ import org.mmartinic.muflon.model.Episode
 
 class EpisodeTagLib {
 
-	def episodeNumber = {attrs ->
-		Episode episode = attrs.episode
-		out << String.format("%02d", episode.seasonNumber)
-		out << "x"
-		out << String.format("%02d", episode.episodeNumber)
-	}
-	
-	def isohuntLink = {attrs ->
-		Episode episode = attrs.episode
-		out << "<a href='http://isohunt.com/torrents/?ihq="
-		out << episode.show.name.encodeAsURL()
-		out << "+S"
-		out << String.format("%02d", episode.seasonNumber)
-		out << "E"
-		out << String.format("%02d", episode.episodeNumber)
-		out << """'><img src="${resource(dir:'images',file:'isohunt.ico')}"></a>"""
-	}
-	
-	def addic7edLink = {attrs ->
-		Episode episode = attrs.episode
-		out << "<a href='http://www.addic7ed.com/search.php?search="
-		out << episode.show.name.encodeAsURL()
-		out << "+S"
-		out << String.format("%02d", episode.seasonNumber)
-		out << "E"
-		out << String.format("%02d", episode.episodeNumber)
-		out << """&Submit=Search'><img src="${resource(dir:'images',file:'addic7ed.ico')}"></a>"""
-	}
+    def episodeNumber = {attrs ->
+        Episode episode = attrs.episode
+        out << String.format("%02d", episode.seasonNumber)
+        out << "x"
+        out << String.format("%02d", episode.episodeNumber)
+    }
+
+    def isohuntLink = {attrs ->
+        Episode episode = attrs.episode
+        out << "<a href='http://isohunt.com/torrents/?ihq="
+        out << episode.show.name.encodeAsURL()
+        out << "+S"
+        out << String.format("%02d", episode.seasonNumber)
+        out << "E"
+        out << String.format("%02d", episode.episodeNumber)
+        out << """'><img src="${resource(dir:'images',file:'isohunt.ico')}">Isohunt</a>"""
+    }
+
+    def addic7edLink = {attrs ->
+        Episode episode = attrs.episode
+        out << "<a href='http://www.addic7ed.com/search.php?search="
+        out << episode.show.name.encodeAsURL()
+        out << "+S"
+        out << String.format("%02d", episode.seasonNumber)
+        out << "E"
+        out << String.format("%02d", episode.episodeNumber)
+        out << """&Submit=Search'><img src="${resource(dir:'images',file:'addic7ed.ico')}">Addic7ed</a>"""
+    }
 }
